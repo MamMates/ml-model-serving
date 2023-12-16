f6a8f87f7a1f177c4a5b81aed8d4427b1f26205b
Putu Widyantara Artanta Wibawa <putuwaw973@gmail.com>
2023-12-16 15:21:50 +0800

    [feat] change response data and error handling

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from .default_response import GlobalResponse, Predict
 from .utils import get_price_data
 from dotenv import load_dotenv
 import numpy as np
+import traceback
 import httpx
 import json
 import cv2
@@ -24,13 +25,19 @@ async def root() -> Response:
 
 
 @app.post("/predict")
-async def predict(image: UploadFile, province: str, name: str | None = '', environment: str | None = 'campus') -> Response:
+async def predict(
+        image: UploadFile,
+        province: str,
+        name: str | None = '',
+        environment: str | None = 'campus'
+) -> Response:
     try:
         contents = image.file.read()
         with open(image.filename, 'wb') as f:
             f.write(contents)
     except BaseException:
-        r = GlobalResponse.DefaultBadRequest()
+        error = traceback.format_exc()
+        r = GlobalResponse.DefaultBadRequest(error=error)
         return Response(
             status_code=r.code,
             content=r.model_dump_json(),
@@ -45,57 +52,66 @@ async def predict(image: UploadFile, province: str, name: str | None = '', envir
         image = cv2.resize(image, (150, 150))
         image = image / 255.0
     except BaseException:
-        r = GlobalResponse.DefaultBadRequest()
+        error = traceback.format_exc()
+        r = GlobalResponse.DefaultBadRequest(error=error)
         return Response(
             status_code=r.code,
             content=r.model_dump_json(),
             media_type="application/json"
         )
 
-    r = GlobalResponse.DefaultOK()
-    p = Predict()
+    try:
+        r = GlobalResponse.DefaultOK(data={})
+        p = Predict()
 
-    clf_url = os.getenv("CLF_ENDPOINT")
-    rating_url = os.getenv("RATING_ENDPOINT")
-    price_url = os.getenv("PRICE_ENDPOINT")
+        clf_url = os.getenv("CLF_ENDPOINT")
+        rating_url = os.getenv("RATING_ENDPOINT")
+        price_url = os.getenv("PRICE_ENDPOINT")
 
-    input_img = json.dumps({"instances": [image.tolist()]})
+        input_img = json.dumps({"instances": [image.tolist()]})
 
-    # food classification
-    response = httpx.post(
-        clf_url,
-        data=input_img,
-        headers={"content-type": "application/json"}
-    )
-    predictions = json.loads(response.text)
-    pred_idx = np.argmax(predictions['predictions'][0])
-    p.category = int(pred_idx)
+        # food classification
+        response = httpx.post(
+            clf_url,
+            data=input_img,
+            headers={"content-type": "application/json"}
+        )
+        predictions = json.loads(response.text)
+        pred_idx = np.argmax(predictions['predictions'][0])
+        p.category = int(pred_idx)
 
-    # food rating
-    response = httpx.post(
-        rating_url,
-        data=input_img,
-        headers={"content-type": "application/json"}
-    )
-    predictions = json.loads(response.text)
-    pred_idx = np.argmax(predictions['predictions'][0])
-    p.rating = int(pred_idx)
+        # food rating
+        response = httpx.post(
+            rating_url,
+            data=input_img,
+            headers={"content-type": "application/json"}
+        )
+        predictions = json.loads(response.text)
+        pred_idx = np.argmax(predictions['predictions'][0])
+        p.rating = int(pred_idx)
 
-    # food price
-    data = get_price_data(province, p.rating, name, environment)
-    response = httpx.post(
-        price_url,
-        data=json.dumps({"instances": data.tolist()}),
-        headers={"content-type": "application/json"}
-    )
-    predictions = json.loads(response.text)
-    pred_result = predictions['predictions'][0][0]
-    p.price = int(pred_result)
+        # food price
+        data = get_price_data(province, p.rating, name, environment)
+        response = httpx.post(
+            price_url,
+            data=json.dumps({"instances": data.tolist()}),
+            headers={"content-type": "application/json"}
+        )
+        predictions = json.loads(response.text)
+        pred_result = predictions['predictions'][0][0]
+        p.price = int(pred_result)
 
-    r.data = p.model_dump()
-
-    return Response(
-        status_code=r.code,
-        content=r.model_dump_json(),
-        media_type="application/json"
-    )
+        r.data = p.model_dump()
+        return Response(
+            status_code=r.code,
+            content=r.model_dump_json(),
+            media_type="application/json"
+        )
+    except BaseException:
+        error = traceback.format_exc()
+        r = GlobalResponse.DefaultBadRequest(error=error)
+        return Response(
+            status_code=r.code,
+            content=r.model_dump_json(),
+            media_type="application/json"
+        )
